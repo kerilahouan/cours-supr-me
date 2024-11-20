048e1a42e62f2ad3b11efa4c756fdd1ae51ce3a7
kerilahouan <kerilahouandjinou@gmail.com>
2024-11-20 15:02:31 +0700

    Add files via upload

---
+++ index7b17.php
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<?xml-stylesheet href="/plugins/system/jce/css/content.css?038621b3375d8f5402d0509550eebcf5" type="text/css"?>
+<feed xmlns="http://www.w3.org/2005/Atom"  xml:lang="fr-fr">
+	<title type="text">Cour suprême du Bénin</title>
+	<subtitle type="text"></subtitle>
+	<link rel="alternate" type="text/html" href="https://www.coursupreme.bj"/>
+	<id>https://www.coursupreme.bj/index.php</id>
+	<updated>2024-11-20T06:16:51+00:00</updated>
+	<generator uri="https://www.joomla.org">Joomla! - Open Source Content Management</generator>
+	<link rel="self" type="application/atom+xml" href="https://www.coursupreme.bj/index.php?format=feed&amp;type=atom"/>
+</feed>
---
+++ indexc0d0.php
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<?xml-stylesheet href="/plugins/system/jce/css/content.css?038621b3375d8f5402d0509550eebcf5" type="text/css"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<title>Cour suprême du Bénin</title>
+		<description><![CDATA[]]></description>
+		<link>https://www.coursupreme.bj/index.php</link>
+		<lastBuildDate>Wed, 20 Nov 2024 06:16:50 +0000</lastBuildDate>
+		<generator>Joomla! - Open Source Content Management</generator>
+		<atom:link rel="self" type="application/rss+xml" href="https://www.coursupreme.bj/index.php?format=feed&amp;type=rss"/>
+		<language>fr-fr</language>
+	</channel>
+</rss>
